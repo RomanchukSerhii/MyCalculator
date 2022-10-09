c97fed001eb8dd23b455bbd1dcfe522adee4b069
Romanchuk Serhii <serhiiromanchuk@gmail.com>
2022-10-09 16:38:47 +0300

    Creating project and update UI

---
+++ app/src/main/java/com/gmail/serhiiromanchuk/mycalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.serhiiromanchuk.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
